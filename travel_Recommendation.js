628ea608aa07c0a18fc9e702b9052f7cd1c5591b
Travis B <SirVreally@outlook.com>
2025-09-16 19:27:14 -0400

    updated contact us for functionality, added programs to travel_recommendation.js to use api

--- travel_Recommendation.js
+++ travel_Recommendation.js
@@ -1,93 +1,49 @@
-const btnSearch = document.getElementById('btnSearch');
-const patients = [];
-
-function addPatient() {
-    const name = document.getElementById("name").value;
-    const gender = document.querySelector('input[name="gender"]:checked');
-    const age = document.getElementById("age").value;
-    const condition = document.getElementById("condition").value;
-
-    if (name && gender && age && condition) {
-      patients.push({ name, gender: gender.value, age, condition });
-      resetForm();
-      generateReport();
-    }
+function addSubmission() {
+  // Get the message of the new Submission
+  let submission = document.getElementById("new_submission");
+  // If the user has left a Submission, display a pop-up
+  if (submission.value != null && submission.value.trim() != "") {
+    console.log("We have received your submission.");
+    //Call showPopup here
+    showPopup(true);
+    
+    // Reset the value of the textarea
+    submission.value = "";
+  }
 }
 
-function resetForm() {
-    document.getElementById("name").value = "";
-    document.querySelector('input[name="gender"]:checked').checked = false;
-    document.getElementById("age").value = "";
-    document.getElementById("condition").value = "";
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
 }
-
-function generateReport() {
-    const numPatients = patients.length;
-    const conditionsCount = {
-      Diabetes: 0,
-      Thyroid: 0,
-      "High Blood Pressure": 0,
-    };
-    const genderConditionsCount = {
-      Male: {
-        Diabetes: 0,
-        Thyroid: 0,
-        "High Blood Pressure": 0,
-      },
-      Female: {
-        Diabetes: 0,
-        Thyroid: 0,
-        "High Blood Pressure": 0,
-      },
-    };
-
-    for (const patient of patients) {
-      conditionsCount[patient.condition]++;
-      genderConditionsCount[patient.gender][patient.condition]++;
-    }
-
-    report.innerHTML = `Number of patients: ${numPatients}<br><br>`;
-    report.innerHTML += `Conditions Breakdown:<br>`;
-    for (const condition in conditionsCount) {
-      report.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`;
-    }
-
-    report.innerHTML += `<br>Gender-Based Conditions:<br>`;
-    for (const gender in genderConditionsCount) {
-      report.innerHTML += `${gender}:<br>`;
-      for (const condition in genderConditionsCount[gender]) {
-        report.innerHTML += `&nbsp;&nbsp;${condition}: ${genderConditionsCount[gender][condition]}<br>`;
-      }
-    }
-}
-
-addPatientButton.addEventListener("click", addPatient);
-
-function searchCondition() {
-    const input = document.getElementById('conditionInput').value.toLowerCase();
+function searchLocation() {
+    const input = document.getElementById('destinationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
-    fetch('health_analysis.json')
-      .then(response => response.json())
-      .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+    const countries = data.conditions.find(item => item.name.toLowerCase() === input);
 
-        if (condition) {
-          const symptoms = condition.symptoms.join(', ');
-          const prevention = condition.prevention.join(', ');
-          const treatment = condition.treatment;
+    if (condition) {
+        const name = countries.name.join(', ');
+        const imageUrl = countries.imageUrl.join(', ');
+        const description = countries.description;
 
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+        resultDiv.innerHTML += `<h2>${countries.name}</h2>`;
+        resultDiv.innerHTML += `<img src="${countries.imageUrl}" alt="hjh">`;
 
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        resultDiv.innerHTML += `<p><strong>Name:</strong> ${Name}</p>`;
+        resultDiv.innerHTML += `<p><strong>Image:</strong> ${imageUrl}</p>`;
+        resultDiv.innerHTML += `<p><strong>Description:</strong> ${description}</p>`;
         } else {
-          resultDiv.innerHTML = 'Condition not found.';
+        resultDiv.innerHTML = 'Country not found.';
         }
-      })
+    })
       .catch(error => {
         console.error('Error:', error);
         resultDiv.innerHTML = 'An error occurred while fetching data.';
